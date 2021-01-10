b819f184c55aa3a9f5196f3de506a18c1fc96e0b
cnnay <carlaalraccn2018@gmail.com>
2021-01-09 21:02:43 -0300

    projeto-jogo dos numeros v1

---
+++ script.js
@@ -0,0 +1,43 @@
+
+alert("O jogo começou!")
+
+let j1 = prompt("Bem vindo Jogador1 digite seu nome: ")
+let t1 = parseInt(prompt('digite um valor'))
+
+let nomej1 = document.getElementById("j1")
+nomej1.innerHTML = j1
+
+let num1 = document.getElementById("n1")
+num1.innerHTML = t1
+
+alert("Carregando...")
+
+let j2 = prompt("Bem vindo Jogador2 digite seu nome: ")
+let t2 = parseInt(prompt('digite outro valor'))
+
+let nomej2 = document.getElementById("j2")
+nomej2.innerHTML = j2
+
+let num2 = document.getElementById("n2")
+num2.innerHTML = t2
+
+
+
+let vitoria = "Jogador "
+let empate = "Jogo empatado "
+let erro = "Números incorretos, comece novamente"
+
+if(t1 > t2){
+    let vencedor = document.getElementById("vencedor")
+    vencedor.innerHTML = vitoria + j1
+} else if(t2 > t1){
+    let vencedor = document.getElementById("vencedor")
+    vencedor.innerHTML = vitoria + j2
+} else if(t1 == t2) {
+    let vencedor = document.getElementById("vencedor")
+    vencedor.innerHTML = empate
+} else{
+    let vencedor = document.getElementById("vencedor")
+    vencedor.innerHTML = erro
+}
+
